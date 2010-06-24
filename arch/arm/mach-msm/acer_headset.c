d0f8437a22515c0741afc862c2d4a7d642bf7739
HUSSON Pierre-Hugues <phhusson@free.fr>
2010-06-24 22:50:26 +0200

    Revert "(Ugly) Rename acer headset driver to h2w, so that android userland take it in account"
    This reverts commit d9f085fa732bc21896ef94ba0a7e6096d4103f4a.

--- arch/arm/mach-msm/acer_headset.c
+++ arch/arm/mach-msm/acer_headset.c
@@ -232,8 +232,7 @@ static int acer_hs_probe(struct platform_device *pdev)
 	hr->debounce_time = ktime_set(0, 500000000);  /* 500 ms */
 
 	INIT_WORK(&short_wq, acer_update_state_work);
-	//hr->sdev.name = "acer-hs";
-	hr->sdev.name = "h2w";
+	hr->sdev.name = "acer-hs";
 	hr->sdev.print_name = acer_hs_print_name;
 	hr->sdev.print_state = acer_hs_print_state;
 	hr->headsetOn = false;
